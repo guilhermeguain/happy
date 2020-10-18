c46e840f7a1657f40f5aa4c2d7e7be5801f538ae
Guilherme Guain <guilherme.guain@gmail.com>
2020-10-18 13:58:26 -0300

    added dashboard button in landing page

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -10,15 +10,21 @@ function Landing() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" title="Happy" />
+        <div className="header">
+          <img src={logoImg} alt="Happy" title="Happy" />
+          <div className="location">
+            <strong>São Paulo</strong>
+            <span>São Paulo</span>
+          </div>
+        </div>
+        <Link to="/dashboard" className="enter-dashboard">
+          Acesso restrito
+        </Link>
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
-        <div className="location">
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
+        
         <Link to="/app" className="enter-app">
           <FiArrowRight size="26" color="rgba(0, 0, 0, 0.6)" />
         </Link>
